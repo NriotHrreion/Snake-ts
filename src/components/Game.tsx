1c5fd3a5e3b97d62261f16f11e4b3737120d6161
NriotHrreion <nriot233@gmail.com>
2021-11-28 10:34:55 +0800

    fix: When the snake died, press 'space' can continue the game

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -63,6 +63,9 @@ export default class Game<P> extends Component<{}, GameState> {
 
         var gameStopEvent = new CustomEvent("gameStop");
         document.body.dispatchEvent(gameStopEvent);
+
+        var gameResetEvent = new CustomEvent("gameReset");
+        document.body.dispatchEvent(gameResetEvent);
     }
 
     public setSpeed(speed: number): void {
